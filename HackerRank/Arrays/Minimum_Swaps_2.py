2abb3979f060b84ba8d118b39595133248bfd2dc
Mateus <matucavieira@gmail.com>
2020-11-22 22:21:23 -0300

    HackerRank - 5 Commit - Linux

--- HackerRank/Arrays/Minimum_Swaps_2.py
+++ HackerRank/Arrays/Minimum_Swaps_2.py
@@ -1,43 +1,11 @@
-'''
-n = list(map(lambda x: False, range(int(input()))))
+n = int(input())
 l = list(map(int, input().split()))
-r, m, p, t = 0, 0, 0, []
-for v in range(len(l)):
-    if n[p] == False and l[p] != p+1:
-        n[p] = True
-        t.append(p)
-        p = l[p]-1
+r = 0
+
+for p in range(n):
+    while l[p] != p+1:
+        x = int(l[p])
+        l[p] = l[x-1]
+        l[x-1] = x
         r += 1
-        if p in t:
-            m -= 1
-            if n.count(False) == 0:
-                break
-            p = n.index(False)
-            t.clear()
-    elif n[p] == False and l[p] == p+1:
-        n[p] = True
-        if n.count(False) == 0:
-            break
-        p = n.index(False)
-
-print(r+m if m != 0 else r-1)
-'''
-
-# 2 3 4 1 5
-
-# Second Attempt
-n = list(range(int(input())+1))
-l = list(map(int, input().split()))
-r = p = 0
-while len(n) != 1:
-    if l[p] != p+1:
-        n.remove(p)
-        p = l[p]-1
-        if p not in n:
-            p = min(n)
-            r -= 1
-        r += 1
-    else:
-        n.remove(p)
-        p = min(n)
 print(r)
